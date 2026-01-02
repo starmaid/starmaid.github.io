cb4f29d0967459e50b75f606c7f4256a492f6283
starmaid <nicholas.masso.14@gmail.com>
2026-01-01 16:39:25 -0800

    add image display to post list

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@
 source "https://rubygems.org"
 gemspec
 
+gem "jekyll"
 gem "webrick", "~> 1.8"
 gem "kramdown-parser-gfm", "~> 1.1"
--- shell.nix
+++ shell.nix
@@ -2,9 +2,14 @@ let
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
   packages = with pkgs; [
-    jekyll
-    bundler
+    ruby.devEnv
     exiftool
     ffmpeg_6  
   ];
+  shellHook = ''
+    export GEM_HOME=$PWD/vendor/bundle
+    export GEM_PATH=$GEM_HOME
+    export BUNDLE_PATH=$GEM_HOME
+    export PATH=$GEM_HOME/bin:$PATH
+  '';
 }
